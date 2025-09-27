02c808c5aebdfeb693669c2e56a046c23848fbb4
Roshan-Singh-06 <singh06xyz@gmail.com>
2025-09-27 21:59:11 +0530

    ehance the code

--- Admin-frontend/src/api/axiosInstance.js
+++ Admin-frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api", "https://rewear-r2vt.onrender.com"// Your backend API base
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5001/api", // Your backend API base
   withCredentials: true, // For cookies/sessions
 });
 
